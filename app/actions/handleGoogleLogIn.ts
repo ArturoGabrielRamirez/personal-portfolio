ba973fa9b145df4f4e9a75e92be468e0f7764246
ArturoGabrielRamirez <jiamafox@gmail.com>
2025-04-02 18:35:56 -0300

    fix deploy redirect

--- app/actions/handleGoogleLogIn.ts
+++ app/actions/handleGoogleLogIn.ts
@@ -9,7 +9,7 @@ export async function handleGoogleLogIn() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-            redirectTo: 'http://localhost:3000/auth/callback', 
+            redirectTo: `${process.env.NEXTAUTH_URL}/api/auth/callback/google`,
             queryParams: {
                 access_type: 'offline',
                 prompt: 'consent',
